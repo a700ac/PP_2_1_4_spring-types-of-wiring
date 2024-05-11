4589adb4bf9dd6f19b32ed840fdb190108779024
a700ac <a700ac@gmail.com>
2024-05-11 16:17:56 +0300

    ToCheckUp

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,13 @@
 package koschei.models;
 
 public class Egg6 {
+    private final Needle7 needle7 = new Needle7();
+
+    public Egg6(Needle7 needle7) {
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return (", в яйце иголка " + needle7.toString());
     }
 }
