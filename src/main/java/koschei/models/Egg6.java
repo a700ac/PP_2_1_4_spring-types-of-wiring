67e685e4fbbd690d74b5c38032c3ac3ed82a73f7
a700ac <a700ac@gmail.com>
2024-05-13 14:37:44 +0300

    ToCheckUp2

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,14 @@
 package koschei.models;
 
-public class Egg6 {
-    private final Needle7 needle7 = new Needle7();
 
+public class Egg6 {
+
+    private Needle7 needle7;
+    // @Autowired можно и сверху и снизу,
+    // но в условии просили еще @Bean на методе в AppConfig,
+    // а без конструктора не получается
     public Egg6(Needle7 needle7) {
+        this.needle7 = needle7;
     }
 
     @Override
